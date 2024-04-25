3d163636fd39454427d3651bc71fc4bdc8681cc8
João Pedro Portocarrero <134610588+SupremeSith@users.noreply.github.com>
2024-04-25 07:57:20 -0300

    Update _layout.tsx

--- my-app/app/TabNav/DrawerNav/Home/_layout.tsx
+++ my-app/app/TabNav/DrawerNav/Home/_layout.tsx
@@ -1,15 +1,20 @@
 import { DrawerToggleButton } from "@react-navigation/drawer";
+
 import { Stack } from "expo-router";
+
 import { useColor } from "../../../../temas/temas";
 
 export default function layout() {
+    
     const cores = useColor();
     return (
+        
         <Stack>
             <Stack.Screen
                 name="Lista"
                 options={{
                     headerLeft: () => <DrawerToggleButton />,
+                    
                     headerSearchBarOptions: { placeholder: "Pesquisar" },
                     headerStyle: { backgroundColor: cores.bgPatrimonio },
                     headerTitleAlign: 'center',
