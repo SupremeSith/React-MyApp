da5d2eea2901fcfb0635b5a35099d8c7fd265787
João Pedro Portocarrero <134610588+SupremeSith@users.noreply.github.com>
2024-04-25 07:52:31 -0300

    Update Additens.tsx

--- my-app/app/TabNav/DrawerNav/Home/Additens.tsx
+++ my-app/app/TabNav/DrawerNav/Home/Additens.tsx
@@ -28,22 +28,28 @@ const AddIten = () => {
 };
 
 const styles = StyleSheet.create({
+    
     container: {
+        
         flex: 1,
     },
     text: {
+        
         color: "#ffff",
         fontSize: 16,
 
     },
     inputContainer: {
+        
         flex: 1,
         marginTop: 50,
     },
     addButtonContainer: {
+        
         position: "absolute",
         bottom: 20,
         right: 0,
+        
         margin: 20,
         marginBottom: 50,
         height: 60,
