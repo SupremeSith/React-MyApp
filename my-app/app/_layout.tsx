68a3fe461efcd9d188a53a72aa6e887112a0017f
João Pedro Portocarrero <134610588+SupremeSith@users.noreply.github.com>
2024-04-25 07:50:14 -0300

    Update _layout.tsx

--- my-app/app/_layout.tsx
+++ my-app/app/_layout.tsx
@@ -5,28 +5,33 @@ export default function layout() {
     <Stack>
 
       <Stack.Screen name="index" options={{
+      
         headerShown: false, 
         statusBarColor: "black" }} />
       
       <Stack.Screen name="TelasIniciais/RecupSenha" options={{
+      
         headerTitle: "" ,
         statusBarColor: "#011E83",
         headerTintColor: "#fff", 
         headerTransparent: true }} />
 
       <Stack.Screen name="TelasIniciais/Cadastro" options={{
+      
         headerTitle: "" , 
         statusBarColor: "red",
         headerTintColor: "#fff",
         headerTransparent: true }} />
 
     <Stack.Screen name="TelasIniciais/Login" options={{
+      
         headerTitle: "" , 
         statusBarColor: "red",
         headerTintColor: "#fff",
         headerTransparent: true }} />
 
       <Stack.Screen name="TabNav" options={{
+      
         headerShown: false, 
       }} />
     </Stack>
