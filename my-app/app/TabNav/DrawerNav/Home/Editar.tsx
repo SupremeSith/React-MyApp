a6ee61290fdcb9b44bfb9a2657fba8f144a07594
João Pedro Portocarrero <134610588+SupremeSith@users.noreply.github.com>
2024-04-25 07:56:37 -0300

    Update Editar.tsx

--- my-app/app/TabNav/DrawerNav/Home/Editar.tsx
+++ my-app/app/TabNav/DrawerNav/Home/Editar.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+
 import { View, StyleSheet, TouchableOpacity, Text} from "react-native";
+
 import PatriBar from "@components/PatrimonioBar";
+
 import {AntDesign, MaterialIcons } from '@expo/vector-icons';
+
 import InputinventoryInput from "@components/Inputitens";
+
 import { Link } from "expo-router";
 
 const Editar = () => {
@@ -21,6 +26,7 @@ const Editar = () => {
             </PatriBar>
 
             <View style={styles.inputContainer}>
+                
                 <InputinventoryInput label="N° Inventario" placeholder="957689"/>
                 <InputinventoryInput label="Descrição" placeholder="CADEIRA GIRATÓRIA"/>
                 <InputinventoryInput label="Local" placeholder="SALA C13"/>
@@ -33,14 +39,17 @@ const Editar = () => {
 };
 
 const styles = StyleSheet.create({
+    
     container: {
         flex: 1,
     },
     text: {
+        
         color: "#ffff",
         fontSize: 16,
     },
     inputContainer: {
+        
         flex: 1,
         marginTop: 50,
 
